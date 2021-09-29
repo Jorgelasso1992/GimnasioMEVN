b11d21065ad75e71061fb7346ed3602c817aa353
ArielBurgos <arielburgos0224@gmail.com>
2021-09-28 23:19:02 -0500

    crear vista en bootstrap y pasarla a vue

---
+++ adelantoBootstrap/client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <registrarcliente></registrarcliente>
+  </div>
+</template>
+
+<script>
+import registrarcliente from './components/registrarcliente.vue';
+export default{
+  components: {registrarcliente},
+  name:"App"
+}
+
+</script>
+<style>
+@import '../assets/bootstrap/css/bootstrap.min.css';
+@import '../assets/css/Login-Form-Clean.css';
+@import '../assets/css/Registratin-Form-with-Photo.css';
+@import '../assets/css/styles.css';
+</style>
---
+++ adelantoBootstrap/client/src/components/registrarcliente.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <v-app>
+        <section class="register-photo">
+            <div class="form-container">
+                <div class="image-holder"></div>
+                <form method="post">
+                    <h2 class="text-center"><strong>Crea</strong>&nbsp;una cuenta.</h2>
+                    <div class="mb-3"><input class="form-control" type="email" name="names" placeholder="Nombres"></div>
+                    <div class="mb-3"><input class="form-control" type="email" name="lastname" placeholder="Apellidos"></div>
+                    <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Correo"></div>
+                    <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Contraseña"></div>
+                    <div class="mb-3"><input class="form-control" type="password" name="password-repeat" placeholder="Contraseña(repetir)"></div>
+                    <div class="mb-3">
+                        <div class="form-check"><label class="form-check-label"><input class="form-check-input" type="checkbox">Acepto los términos y condiciones&nbsp;</label></div>
+                    </div>
+                    <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Ingresar</button></div><a class="already" href="#">Ya tienes una cuenta registrada? Ingresa aquí.</a>
+                </form>
+            </div>
+        </section>
+    </v-app>   
+</template>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script>
+export default {
+    name:"registrarcliente"
+}
+</script>
