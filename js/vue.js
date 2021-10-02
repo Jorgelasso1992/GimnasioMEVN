b6abdfb97893fd19a240163049f1ee3f8303cdc4
michaelcifuentes10 <90076782+michaelcifuentes10@users.noreply.github.com>
2021-10-01 23:27:39 -0500

    Add files via upload

---
+++ js/vue.js
@@ -0,0 +1,35 @@
+var myObject = new Vue({
+	el: '#app',
+	data: {message: 'Nuestras Sedes'}
+  })
+
+var myObject = new Vue({
+	el: '#app1',
+	data: {message: 'Equipos Disponibles'}
+  })
+
+var myObject = new Vue({
+	el: '#app2',
+	data: {message: 'Planes de pago'}
+  })
+
+var myObject = new Vue({
+	el: '#app3',
+	data: {message: 'Noticias, recomendaciones, bioseguridad y nutrición'}
+  })
+
+var myObject = new Vue({
+	el: '#app4',
+	data: {message: 'Galería'}
+  })
+
+
+var myObject = new Vue({
+	el: '#app5',
+	data: {message: 'INICIAR SESIÓN'}
+  })
+
+var myObject = new Vue({
+	el: '#app6',
+	data: {message: 'REGISTRARSE'}
+  })
