137008d156c73f6ebcded0e86a0371007d1847a4
Jorgelasso1992 <jorgelasso_1992@hotmail.com>
2021-10-09 11:57:35 -0500

    Primeros pasos conexión VUE con API REST y MONGODB

---
+++ cliente/entrenador/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <h1>Lista de entrenadores</h1>
+    <table class="table table-striped table-active">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Documento Identidad</th>
+          <th scope="col">Nombres</th>
+          <th scope="col">Apellidos</th>
+          <th scope="col">Email</th>
+          <th scope="col">Teléfono</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+};
+</script>
--- cliente/entrenador/src/views/registroentrenador.vue
+++ cliente/entrenador/src/views/registroentrenador.vue
@@ -204,7 +204,6 @@ export default {
   name: "registroentrenador",
   data: () => ({}),
   methods: {},
-  created() {},
 };
 </script>
 
